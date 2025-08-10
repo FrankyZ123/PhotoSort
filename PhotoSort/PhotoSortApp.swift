915c0a23789cc7850888842aa328928ad3ee72d5
Zane Sand <zane@palomalearning.com>
2025-08-10 05:44:24 -0700

    Initial Commit

---
+++ PhotoSort/PhotoSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSortApp.swift
+//  PhotoSort
+//
+//  Created by Zane Sand on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoSortTests/PhotoSortTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSortTests.swift
+//  PhotoSortTests
+//
+//  Created by Zane Sand on 8/10/25.
+//
+
+import Testing
+@testable import PhotoSort
+
+struct PhotoSortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
